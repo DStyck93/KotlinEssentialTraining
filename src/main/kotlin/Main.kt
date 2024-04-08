1a9803d9cd0d925fc19e687c1c8833d95df90c96
DStyck93 <DStyck93@gmail.com>
2024-04-08 16:30:53 -0500

    Challenge 2: Refactor a helper class to use top-level functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
+//    ListUtils.nonEmptyList(arrayOf(1, 2, 3))
+//    ListUtils.setToNonEmptyList(setOf("a", "b", "c"))
 
+    arrayOf(1, 2, 3).nonEmptyList()
+    setOf("a", "b", "c").toNonEmptyList()
 }
