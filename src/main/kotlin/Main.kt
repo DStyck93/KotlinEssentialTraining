244c58927e76045f7ef79ce44fa5064165b9cf85
DStyck93 <DStyck93@gmail.com>
2024-04-08 14:53:43 -0500

    Object Classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
-
 fun main() {
+//    println(NetworkConfig.baseUrl)
 
+//    println(NetworkConfig.getNetworkDetails())
+
+    val provider: NetworkInfoProvider = object : NetworkInfoProvider {
+        override fun getNetworkDetails(): String {
+            TODO("Not yet implemented")
+        }
+    }
+    provider.getNetworkDetails()
 }
