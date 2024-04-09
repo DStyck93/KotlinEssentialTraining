c6ff3f467ad7d52e633a2ba0521492812f924fa6
DStyck93 <DStyck93@gmail.com>
2024-04-09 13:28:07 -0500

    Threads, thread pools, and executors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
-fun main() {
+import java.util.concurrent.Executors
 
+class CustomThread : Thread("Custom Thread") {
+    override fun run() {
+        super.run()
+        println(Thread.currentThread().name)
+        println("CustomThread.run()")
+    }
+}
+
+class CustomRunnable : Runnable {
+    override fun run() {
+        println(Thread.currentThread().name)
+        println("CustomRunnable.run()")
+    }
+}
+
+val executor = Executors.newSingleThreadExecutor()
+val multiThreadExecutor = Executors.newFixedThreadPool(3)
+
+fun main() {
+    println(Thread.currentThread().name)
+//    CustomThread().start()
+
+//    Thread(CustomRunnable()).start()
+
+//    executor.submit(CustomRunnable())
+
+    for (i in 0..10) {
+        multiThreadExecutor.submit(CustomRunnable())
+    }
 }
